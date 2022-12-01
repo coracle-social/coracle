4b59484cb7a0d635196f1dfd818fd536cb912512
Jonathan Staab <shtaab@gmail.com>
2022-11-30 21:14:06 -0800

    Add spinner component

---
+++ src/partials/Spinner.svelte
@@ -0,0 +1,8 @@
+<script>
+  import {fade} from 'svelte/transition'
+  import {Circle2} from 'svelte-loading-spinners'
+</script>
+
+<div class="py-20 flex justify-center" in:fade={{delay: 1000}}>
+  <Circle2 colorOuter="#CCC5B9" colorInner="#403D39" colorCenter="#EB5E28" />
+</div>
