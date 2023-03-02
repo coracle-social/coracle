ad4be88d87c77d6677389ec94deb681f9b3aad05
fiatjaf <fiatjaf@gmail.com>
2023-03-02 08:44:27 -0300

    support nip05 addresses on advanced search.

--- src/views/search/Scan.svelte
+++ src/views/search/Scan.svelte
@@ -3,18 +3,40 @@
   import {onDestroy} from 'svelte'
   import {navigate} from 'svelte-routing'
   import {waitFor} from 'hurdak/lib/hurdak'
+  import {any, flip, startsWith} from 'ramda'
+  import {nip05, nip19} from 'nostr-tools'
   import Input from 'src/partials/Input.svelte'
   import Anchor from 'src/partials/Anchor.svelte'
   import Spinner from 'src/partials/Spinner.svelte'
+  import {toast} from "src/app/ui"
 
   let mode = 'input', video, ready, value, scanner
 
   const onDecode = result => {
-    goToEntity(result.data)
+    handleInput(result.data)
   }
 
-  const goToEntity = entity => {
-    navigate("/" + entity.replace('nostr:', ''))
+  const handleInput = async input => {
+    input = input.replace('nostr:', '')
+
+    if (any(flip(startsWith)(input), ["note1", "npub1", "nevent1", "nprofile1"])) {
+      navigate("/" + input)
+      return
+    }
+
+    if (input.match(/^[a-f0-9]{64}$/)) {
+      navigate("/" + nip19.npubEncode(input))
+      return
+    }
+
+    let profile = await nip05.queryProfile(input)
+    if (profile) {
+      navigate("/" + nip19.nprofileEncode(profile))
+      return
+    }
+
+    toast.show("warning", "That isn't a valid nostr identifier")
+    return
   }
 
   const setMode = async newMode => {
@@ -46,17 +68,17 @@
 </script>
 
 {#if mode === 'input'}
-<div class="flex gap-2">
+<form class="flex gap-2" on:submit|preventDefault={() => handleInput(value)}>
   <Input placeholder="nprofile..." bind:value={value} wrapperClass="flex-grow" />
-  <Anchor type="button" on:click={() => goToEntity(value)}>
+  <Anchor type="button" on:click={() => handleInput(value)}>
     <i class="fa fa-arrow-right" />
   </Anchor>
   <Anchor type="button" on:click={() => setMode('scan')}>
     <i class="fa fa-qrcode" />
   </Anchor>
-</div>
+</form>
 <div class="text-center text-light">
-  Enter any nostr identifier (npub, nevent, nprofile or note), or click on the
+  Enter any nostr identifier (npub, nevent, nprofile, note or user@domain.tld), or click on the
   camera icon to scan with your device's camera instead.
 </div>
 {:else}
