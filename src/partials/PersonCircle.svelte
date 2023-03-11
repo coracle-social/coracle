b0f6dbd760bf40bf188eaf0832c08851b7c0bc3e
Wojciech Morkowski <wojciech.morkowski@gmail.com>
2023-03-11 23:20:51 +0100

    clean up generation code

--- src/partials/PersonCircle.svelte
+++ src/partials/PersonCircle.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
-  import { invertColor, stringToHex } from "src/util/misc";
+  import { generateAvatarColors } from "src/util/misc";
   import ImageCircle from "./ImageCircle.svelte";
   import LogoSvg from "./LogoSvg.svelte";
   import cx from "classnames";
+  import type { Person } from "src/util/types"
 
-  export let person;
+  export let person : Person;
   export let size = 4;
 
-  const color = stringToHex(person.pubkey);
-  const bgColor = invertColor(color);
+  $: avatarColor = person.kind0 && !person.kind0.picture
+    ? generateAvatarColors(person.pubkey)
+    : { primary: '#EB5E28', bg: 'transparent' };
 </script>
 
 {#if person.kind0?.picture}
@@ -17,8 +19,8 @@
   <ImageCircle
     {size}
     src={""}
-    class={cx($$props.class, "test", "relative")}
-    style="--logo-color: {color}; --logo-bg-color: {bgColor}; background-color: var(--logo-bg-color);"
+    class={cx($$props.class, "relative")}
+    style="--logo-color: {avatarColor.primary}; --logo-bg-color: {avatarColor.bg}; background-color: var(--logo-bg-color);"
   >
     <LogoSvg class="absolute left-2/4 top-2/4 -translate-x-1/2 -translate-y-1/2 logo" style="height: 85%; width: 85%;" />
   </ImageCircle>
