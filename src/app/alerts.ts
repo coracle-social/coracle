e9e19f5811e071f441536df249699dd3036ee9da
Jonathan Staab <shtaab@gmail.com>
2023-03-13 14:36:54 -0500

    Shorten alerts

--- src/app/alerts.ts
+++ src/app/alerts.ts
@@ -158,7 +158,7 @@ const processChats = async (pubkey, events) => {
 
 const listen = async pubkey => {
   // Include an offset so we don't miss alerts on one relay but not another
-  const since = now() - timedelta(30, 'days')
+  const since = now() - timedelta(7, 'days')
   const roomIds = pluck('id', database.rooms.all({joined: true}))
 
   if (listener) {
