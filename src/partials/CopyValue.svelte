dadc93b28a9562b9ef4769cc40f7bee6e8abbbbe
Jonathan Staab <shtaab@gmail.com>
2023-04-05 15:08:24 -0500

    Add CopyValue

---
+++ src/partials/CopyValue.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import {copyToClipboard} from "src/util/html"
+  import {toast} from "src/app/ui"
+
+  export let label
+  export let value
+
+  const copy = () => {
+    copyToClipboard(value)
+    toast.show("info", `${label} copied to clipboard!`)
+  }
+</script>
+
+<strong>{label}</strong>
+<div class="flex items-center gap-2 font-mono text-sm">
+  <i class="fa-solid fa-copy cursor-pointer" on:click={copy} />
+  {value.length > 80 ? value.slice(0, 50) + "..." : value}
+</div>
