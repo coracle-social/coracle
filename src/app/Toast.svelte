16cc59fc3871fc6d9fd367f0a0b6e627f6cf86a5
Jonathan Staab <shtaab@gmail.com>
2023-08-07 12:53:52 -0700

    Make toast dismissable with touch

--- src/app/Toast.svelte
+++ src/app/Toast.svelte
@@ -3,14 +3,50 @@
   import {is} from "ramda"
   import {fly} from "src/util/transition"
   import {toast} from "src/partials/state"
+
+  let touchStart = 0
+  let startOffset = 0
+  let currentOffset = 0
+
+  $: offset = currentOffset - startOffset
+
+  const onTouchStart = e => {
+    touchStart = Date.now()
+    startOffset = e.touches[0].clientX
+    currentOffset = e.touches[0].clientX
+  }
+
+  const onTouchMove = e => {
+    currentOffset = e.touches[0].clientX
+  }
+
+  const onTouchEnd = e => {
+    if (Math.abs(offset) > 200 || (Math.abs(offset) > 50 && Date.now() - touchStart < 300)) {
+      currentOffset = 2 * (offset > 0 ? window.innerWidth : -window.innerWidth)
+
+      setTimeout(() => {
+        toast.set(null)
+      }, 200)
+    }
+
+    setTimeout(() => {
+      touchStart = 0
+      startOffset = 0
+      currentOffset = 0
+    }, 200)
+  }
 </script>
 
 {#if $toast}
   {#key "key"}
     <div
+      on:touchstart={onTouchStart}
+      on:touchmove={onTouchMove}
+      on:touchend={onTouchEnd}
       class="pointer-events-none fixed left-0 right-0 top-0 z-30 flex justify-center"
       transition:fly={{y: -50, duration: 300}}>
       <div
+        style={`transform: translate(${offset}px, 0)`}
         class={cx(
           "pointer-events-auto m-2 rounded border p-3 text-center shadow-xl sm:ml-2",
           "max-w-xl flex-grow transition-all",
