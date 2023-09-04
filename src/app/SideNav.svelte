8e17c9f1efcf54ad5e78f21d589d0d300f71e6b0
Jonathan Staab <shtaab@gmail.com>
2023-09-04 12:49:08 -0700

    Add privacy policy and ToS

--- src/app/SideNav.svelte
+++ src/app/SideNav.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import cx from "classnames"
   import {theme, installPrompt} from "src/partials/state"
+  import Anchor from 'src/partials/Anchor.svelte'
   import {Keys, Nip28, Nip04, Nip24, Alerts} from "src/app/engine"
   import {menuIsOpen} from "src/app/state"
 
@@ -112,5 +113,10 @@
     <a class="block px-4 py-2 transition-all hover:bg-accent hover:text-white" href="/about">
       <i class="fa fa-info-circle mr-2" /> About
     </a>
+  </li>
+  <li class="absolute bottom-0 w-full flex justify-center gap-2 p-2 text-sm text-gray-5">
+    <Anchor external theme="anchor" href="/public/terms.html">Terms</Anchor>
+    &bull;
+    <Anchor external theme="anchor" href="/public/privacy.html">Privacy</Anchor>
   </li>
 </ul>
