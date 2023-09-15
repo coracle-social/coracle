d973b1fc7aa380d2e2f1818e6761c9525f0cce66
Jonathan Staab <shtaab@gmail.com>
2023-09-15 08:17:12 -0700

    Await promise for nip 98

--- src/engine2/commands/nip98.ts
+++ src/engine2/commands/nip98.ts
@@ -2,9 +2,9 @@ import {Fetch} from "hurdak"
 import {signer} from "src/engine2/queries"
 import {buildEvent} from "./util"
 
-export function uploadToNostrBuild(body) {
+export const uploadToNostrBuild = async body => {
   const url = "https://nostr.build/api/v2/upload/files"
-  const event = signer.get().prepAsUser(
+  const event = await signer.get().prepAsUser(
     buildEvent(27235, {
       tags: [
         ["u", url],
