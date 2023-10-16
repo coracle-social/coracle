20484ed5ad6052f724657af8eaabb97f1f5a039f
Jonathan Staab <shtaab@gmail.com>
2023-10-16 14:54:22 -0700

    Use css/html instead of js for border sizing

--- src/app/shared/Note.svelte
+++ src/app/shared/Note.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import {last, partition, reject, propEq, uniqBy, prop} from "ramda"
+  import {partition, reject, propEq, uniqBy, prop} from "ramda"
   import {onMount, onDestroy} from "svelte"
   import {quantify, batch} from "hurdak"
   import {Tags, findRootId, isChildOf, findReplyId, isLike} from "src/util/nostr"
@@ -43,6 +43,7 @@
   export let depth = 0
   export let anchorId = null
   export let topLevel = false
+  export let isLastReply = false
   export let showParent = true
   export let showLoading = false
   export let showMuted = false
@@ -59,8 +60,6 @@
   const {ENABLE_ZAPS} = $env
   const showEntire = anchorId === event.id
   const interactive = !anchorId || !showEntire
-
-  let interval, border, childrenContainer, noteContainer
 
   const onClick = e => {
     const target = (e.detail?.target || e.target) as HTMLElement
@@ -95,28 +94,6 @@
     ctx = reject(propEq("id", e.id), ctx)
   }
 
-  const setBorderHeight = () => {
-    const getHeight = e => e?.getBoundingClientRect().height || 0
-
-    if (childrenContainer && noteContainer) {
-      const lastChild = last(
-        [].slice.apply(childrenContainer.children).filter(e => e.matches(".note"))
-      ) as any
-
-      if (lastChild) {
-        const height =
-          66 +
-          getHeight(childrenContainer) -
-          getHeight(lastChild) -
-          getHeight(lastChild.nextElementSibling) -
-          getHeight(lastChild.nextElementSibling?.nextElementSibling) -
-          (lastChild.nextElementSibling ? 16 : 0)
-
-        border.style = `height: ${height - 21}px`
-      }
-    }
-  }
-
   $: muted = !showMuted && $isEventMuted(event, true)
 
   // Find children in our context
@@ -147,8 +124,6 @@
   )
 
   onMount(async () => {
-    interval = setInterval(setBorderHeight, 400)
-
     const zapAddress = Tags.from(note).getMeta("zap")
 
     if (zapAddress) {
@@ -186,7 +161,6 @@
   })
 
   onDestroy(() => {
-    clearInterval(interval)
     unsubZapper?.()
   })
 </script>
@@ -196,12 +170,17 @@
     .at("notes")
     .of(event.id, {relays: getEventHints(event)})
     .toString()}
-  <div class="note">
-    <div bind:this={noteContainer} class="group relative">
+  <div class="note relative" class:py-2={!showParent && !topLevel}>
+    {#if !showParent && !topLevel}
+      <div class="absolute -left-4 h-px w-4 bg-gray-6" style="top: 27px;" />
+      {#if isLastReply}
+        <div class="absolute -left-4 w-px bg-gray-6" style="height: 19px;" />
+      {:else}
+        <div class="absolute -left-4 h-full w-px bg-gray-6" />
+      {/if}
+    {/if}
+    <div class="group relative">
       <Card stopPropagation class="relative flex gap-4" on:click={onClick} {interactive}>
-        {#if !showParent && !topLevel}
-          <div class="absolute z-10 -ml-4 h-px w-4 bg-gray-6" style="left: 0px; top: 27px;" />
-        {/if}
         <div>
           <Anchor class="text-lg font-bold" on:click={showPerson}>
             <PersonCircle class="h-10 w-10" pubkey={event.pubkey} />
@@ -298,21 +277,33 @@
       }} />
 
     {#if visibleReplies.length > 0 || hiddenReplies.length > 0}
-      <div class="relative mt-4">
-        <div class="absolute z-10 -mt-4 ml-4 h-0 w-px bg-gray-6" bind:this={border} />
-        <div class="note-children relative ml-8 flex flex-col gap-4" bind:this={childrenContainer}>
-          {#if !showEntire && unmutedReplies.length > visibleReplies.length}
-            <button class="ml-5 cursor-pointer py-2 text-gray-1 outline-0" on:click={onClick}>
-              <i class="fa fa-up-down pr-2 text-sm" />
-              Show {quantify(
-                unmutedReplies.length - visibleReplies.length,
-                "other reply",
-                "more replies"
-              )}
-            </button>
-          {/if}
-          {#each visibleReplies as r (r.id)}
+      <div class="note-children relative ml-8 mt-2 flex flex-col">
+        {#if !showEntire && unmutedReplies.length > visibleReplies.length}
+          <button class="ml-5 cursor-pointer py-2 text-gray-1 outline-0" on:click={onClick}>
+            <i class="fa fa-up-down pr-2 text-sm" />
+            Show {quantify(
+              unmutedReplies.length - visibleReplies.length,
+              "other reply",
+              "more replies"
+            )}
+          </button>
+        {:else if visibleReplies.length > 0 || (showEntire && showMutedReplies && hiddenReplies.length > 0)}
+          <div class="absolute -left-4 -top-2 h-4 w-px bg-gray-6" />
+        {/if}
+        {#each visibleReplies as r, i (r.id)}
+          <svelte:self
+            isLastReply={!showMutedReplies && i === visibleReplies.length - 1}
+            showParent={false}
+            showMuted
+            note={r}
+            depth={depth - 1}
+            context={ctx}
+            {anchorId} />
+        {/each}
+        {#if showEntire && showMutedReplies}
+          {#each hiddenReplies as r, i (r.id)}
             <svelte:self
+              isLastReply={i === hiddenReplies.length - 1}
               showParent={false}
               showMuted
               note={r}
@@ -320,28 +311,17 @@
               context={ctx}
               {anchorId} />
           {/each}
-          {#if showEntire && showMutedReplies}
-            {#each hiddenReplies as r (r.id)}
-              <svelte:self
-                showParent={false}
-                showMuted
-                note={r}
-                depth={depth - 1}
-                context={ctx}
-                {anchorId} />
-            {/each}
-          {:else if showEntire && hiddenReplies.length > 0}
-            <button
-              class="ml-5 cursor-pointer py-2 text-gray-1 outline-0"
-              in:fly={{y: 20}}
-              on:click={() => {
-                showMutedReplies = true
-              }}>
-              <i class="fa fa-up-down pr-2 text-sm" />
-              Show {quantify(hiddenReplies.length, "hidden reply", "hidden replies")}
-            </button>
-          {/if}
-        </div>
+        {:else if showEntire && hiddenReplies.length > 0}
+          <button
+            class="ml-5 cursor-pointer py-2 text-gray-1 outline-0"
+            in:fly={{y: 20}}
+            on:click={() => {
+              showMutedReplies = true
+            }}>
+            <i class="fa fa-up-down pr-2 text-sm" />
+            Show {quantify(hiddenReplies.length, "hidden reply", "hidden replies")}
+          </button>
+        {/if}
       </div>
     {/if}
   </div>
--- src/app/shared/NoteReply.svelte
+++ src/app/shared/NoteReply.svelte
@@ -91,8 +91,8 @@
     class="note-reply relative z-10 my-2 flex flex-col gap-1"
     bind:this={container}
     on:click|stopPropagation>
-    <div
-      class="overflow-hidden rounded-2xl border border-solid border-gray-7 group-[.modal]:border-gray-6">
+    <div class="absolute bottom-0 left-4 top-0 z-0 -my-2 ml-px w-px bg-gray-6" />
+    <div class="z-10 overflow-hidden rounded-2xl border border-solid border-gray-6">
       <div class="bg-gray-7 p-3 text-gray-2" class:rounded-b={data.mentions.length === 0}>
         <Compose bind:this={reply} onSubmit={send} style="min-height: 4rem">
           <div class="flex flex-col justify-start" slot="addon">
