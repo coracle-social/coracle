884db723942123ae44b3c4282bce9ad3ba61cafb
Jon Staab <shtaab@gmail.com>
2023-11-30 10:59:38 -0800

    Fix person link to be reactive

--- src/app/shared/PersonLink.svelte
+++ src/app/shared/PersonLink.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import Anchor from "src/partials/Anchor.svelte"
   import {router} from "src/app/router"
-  import {displayPubkey, loadPubkeys} from "src/engine"
+  import {displayPerson, derivePerson, loadPubkeys} from "src/engine"
 
   export let pubkey
 
+  const person = derivePerson(pubkey)
   const path = router.at("people").of(pubkey).toString()
 
   loadPubkeys([pubkey])
@@ -12,6 +13,6 @@
 
 <span class={$$props.class}>
   @<Anchor modal theme="anchor" href={path}>
-    {displayPubkey(pubkey)}
+    {displayPerson($person)}
   </Anchor>
 </span>
