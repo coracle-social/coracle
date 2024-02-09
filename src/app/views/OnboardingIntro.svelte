60d407d3282f361b56baa3634e1f66739b482fa1
Jon Staab <shtaab@gmail.com>
2024-02-08 22:03:46 -0800

    Add 30s video to onboarding

--- src/app/views/OnboardingIntro.svelte
+++ src/app/views/OnboardingIntro.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
   import {appName} from "src/partials/state"
   import Anchor from "src/partials/Anchor.svelte"
+  import Media from "src/partials/Media.svelte"
+  import Modal from "src/partials/Modal.svelte"
 
   export let setStage
 
+  let media
+
   const next = () => setStage("profile")
+
+  const openMedia = url => {
+    media = url
+  }
+
+  const closeMedia = () => {
+    media = null
+  }
 </script>
 
 <div class="flex gap-3">
@@ -21,14 +33,16 @@
 </p>
 <div class="flex flex-col gap-2 sm:flex-row">
   <Anchor
-    class="relative flex aspect-[4/3] items-center justify-center overflow-hidden rounded-xl p-8 text-center sm:w-1/2">
+    class="relative flex aspect-[4/3] items-center justify-center overflow-hidden rounded-xl p-8 text-center sm:w-1/2"
+    on:click={() => openMedia("https://coracle.us-southeast-1.linodeobjects.com/coracle-30s.mp4")}>
     <div
       class="absolute inset-0 opacity-75 transition-opacity hover:opacity-100"
       style="background: url('/images/jakob-owens-8tyCOqTqdqg-unsplash.png'" />
-    <p class="staatliches relative text-5xl text-white">{appName} in 30 seconds</p>
+    <p class="staatliches relative text-5xl text-white">Nostr in 30 seconds</p>
   </Anchor>
   <Anchor
-    class="relative hidden aspect-[4/3] items-center justify-center overflow-hidden rounded-xl p-8 text-center sm:flex sm:w-1/2">
+    class="relative hidden aspect-[4/3] items-center justify-center overflow-hidden rounded-xl p-8 text-center sm:flex sm:w-1/2"
+    on:click={() => openMedia("https://dufflepud.us-southeast-1.linodeobjects.com/coracle_intro.mp4")}>
     <div
       class="absolute inset-0 opacity-75 transition-opacity hover:opacity-100"
       style="background: url('/images/sean-105m46GatAg-unsplash.png'" />
@@ -39,3 +53,9 @@
   When you’re ready, click below and we’ll guide you through the process of creating an account.
 </p>
 <Anchor button accent class="text-center" on:click={next}>Let's go!</Anchor>
+
+{#if media}
+  <Modal onEscape={closeMedia}>
+    <Media url={media || ""} />
+  </Modal>
+{/if}
