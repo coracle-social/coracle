33033045484aa1322e86baf9d9ee2ba1a230ffc8
Jon Staab <shtaab@gmail.com>
2024-02-12 11:00:52 -0800

    Revert "Pablo spinner"
    This reverts commit 9bb839bf94999358698c07e01b37aa7eef8e6900.

--- src/partials/Spinner.svelte
+++ src/partials/Spinner.svelte
@@ -10,6 +10,5 @@
 
 <div class="flex flex-col items-center justify-center gap-4 py-20" in:fade|local={{delay}}>
   <slot />
-  <img src="https://pablof7z.com/images/me.jpg" class="fa-spin h-20 w-20 rounded-full" />
-  <!-- <Circle2 colorOuter={accent} colorInner={dark} colorCenter={light} /> -->
+  <Circle2 colorOuter={accent} colorInner={dark} colorCenter={light} />
 </div>
