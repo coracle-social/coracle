66707f646719de7d308d53a0724321890ca1be3f
Jon Staab <shtaab@gmail.com>
2024-02-22 09:36:35 -0800

    Fix sendLegacyMessage

--- src/engine/channels/commands.ts
+++ src/engine/channels/commands.ts
@@ -1,29 +1,28 @@
 import {assoc, without, uniq, map} from "ramda"
 import {createMapOf} from "hurdak"
-import {now} from "paravel"
+import {now, createEvent} from "paravel"
 import {generatePrivateKey, appDataKeys} from "src/util/nostr"
-import {Publisher, publish, getClientTags, mention} from "src/engine/network/utils"
+import {Publisher, getClientTags, mention} from "src/engine/network/utils"
 import {getPubkeyHints} from "src/engine/relays/utils"
-import {user, nip59, nip04} from "src/engine/session/derived"
+import {user, signer, nip59, nip04} from "src/engine/session/derived"
 import {setAppData} from "src/engine/session/commands"
 import {channels} from "./state"
 
 export const sendLegacyMessage = async (channelId: string, content: string) => {
   const recipients = without([user.get().pubkey], channelId.split(","))
 
-  if (recipients.length !== 1) {
+  if (recipients.length > 1) {
     throw new Error("Attempted to send legacy message to more than 1 recipient")
   }
 
   const [pubkey] = recipients
-  const template = {
-    kind: 4,
-    created_at: now(),
+  const template = createEvent(4, {
     content: await nip04.get().encryptAsUser(content, pubkey),
     tags: [mention(pubkey), ...getClientTags()],
-  }
+  })
 
-  return publish(template, {
+  return Publisher.publish({
+    event: await signer.get().signAsUser(template),
     relays: getPubkeyHints(pubkey, "read"),
   })
 }
