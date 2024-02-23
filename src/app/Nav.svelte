74525ee11ee4c02df19f1fad8e25fbab5ed03363
Jon Staab <shtaab@gmail.com>
2024-02-22 17:21:47 -0800

    Update bottom nav styles

--- src/app/Nav.svelte
+++ src/app/Nav.svelte
@@ -84,12 +84,12 @@
 <!-- bottom nav -->
 {#if innerWidth < 1024}
   <div
-    class="fixed bottom-0 left-0 right-0 z-nav flex items-center justify-between border-t border-solid border-neutral-600 bg-neutral-800 px-4 py-2">
+    class="fixed bottom-0 left-0 right-0 z-nav flex items-center justify-between border-neutral-600 bg-tinted-800 dark:bg-black px-4 py-2">
     <div class="w-1/3">
       <div
-        class="h-10 w-10 cursor-pointer rounded-full border-[3px] border-solid border-neutral-600 dark:border-tinted-600 p-1 pl-2.5 pt-1.5 text-accent"
+        class="h-8 w-8 cursor-pointer rounded-full border-[3px] border-solid border-neutral-600 dark:border-tinted-600 text-accent flex justify-center items-center"
         on:click={openSearch}>
-        <i class="fa fa-search scale-[140%]" />
+        <i class="fa fa-search" />
       </div>
     </div>
     <div>
@@ -105,7 +105,7 @@
           <path fill="currentColor" d="M0 88C0 74.7 10.7 64 24 64H424c13.3 0 24 10.7 24 24s-10.7 24-24 24H24C10.7 112 0 101.3 0 88zM0 248c0-13.3 10.7-24 24-24H424c13.3 0 24 10.7 24 24s-10.7 24-24 24H24c-13.3 0-24-10.7-24-24zM448 408c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24s10.7-24 24-24H424c13.3 0 24 10.7 24 24z"/>
         </svg>
         {#if $pubkey}
-          <PersonCircle class="-ml-4 h-12 w-12 border-4 !border-neutral-800" pubkey={$pubkey} />
+          <PersonCircle class="-ml-4 h-10 w-10 border-4 !border-neutral-800" pubkey={$pubkey} />
           {#if $hasNewNotifications || $hasNewMessages}
             <div class="absolute right-4 top-4 h-2 w-2 rounded bg-accent" />
           {/if}
