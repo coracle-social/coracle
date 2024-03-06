7a80ac53ff2b6a0d682153267df55134ca57008e
Jon Staab <shtaab@gmail.com>
2024-03-06 09:11:23 -0800

    Bump paravel

--- src/engine/lists/derived.ts
+++ src/engine/lists/derived.ts
@@ -9,6 +9,17 @@ export const lists = derivedCollection<List>("address", [_lists, deletes], ([$l,
   $l.filter(l => !$d.has(l.address)),
 )
 
-export const userLists = derivedCollection<List>("address", [lists, pubkey], ([$l, $pk]) =>
-  sortBy((l: any) => (l.title || l.name).toLowerCase(), $l.filter(whereEq({pubkey: $pk}))),
-)
+export const userLists = derivedCollection<List>("address", [lists, pubkey], ([$l, $pk]) => {
+  const m = new Map()
+  const getName = (l: any) => (l.title || l.name).toLowerCase()
+
+  for (const list of $l.filter(whereEq({pubkey: $pk}))) {
+    const name = getName(list)
+
+    if (!m.has(name) || list.address.startsWith("30003")) {
+      m.set(name, list)
+    }
+  }
+
+  return sortBy(getName, Array.from(m.values()))
+})
