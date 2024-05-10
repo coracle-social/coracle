81c48a1774cb3564cfc99747b0e3567d7ce09afd
Jon Staab <shtaab@gmail.com>
2024-05-10 11:13:20 -0700

    Add link to feed card

--- src/app/shared/FeedCard.svelte
+++ src/app/shared/FeedCard.svelte
@@ -2,15 +2,23 @@
   import {Kind} from '@welshman/util'
   import FlexColumn from "src/partials/FlexColumn.svelte"
   import Card from "src/partials/Card.svelte"
+  import Anchor from "src/partials/Anchor.svelte"
   import FeedSummary from "src/app/shared/FeedSummary.svelte"
   import {readFeed, displayFeed, listAsFeed} from "src/domain"
   import {repository, lists} from 'src/engine'
+  import {feed as globalFeed} from 'src/app/state'
+  import {router} from 'src/app/util'
 
   export let address
 
   const feed = address.startsWith(Kind.ListBookmarks)
     ? listAsFeed(lists.key(address).get())
     : readFeed(repository.getEvent(address))
+
+  const loadFeed = () => {
+    globalFeed.set(feed)
+    router.at('notes').push()
+  }
 </script>
 
 <Card>
@@ -18,9 +26,9 @@
     <div class="flex items-center justify-between">
       <span class="staatliches flex items-center gap-3 text-xl">
         <i class="fa fa-rss" />
-        <span class:text-neutral-500={!feed.name}>
+        <Anchor on:click={loadFeed} class={feed.name ? "" : "text-neutral-500"}>
           {displayFeed(feed)}
-        </span>
+        </Anchor>
       </span>
       <slot name="controls" />
     </div>
