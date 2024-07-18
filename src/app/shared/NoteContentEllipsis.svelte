9ccc8408a294961b12bef681ee7cb1f85d8862bd
Jon Staab <shtaab@gmail.com>
2024-07-18 13:10:10 -0700

    remove see more underline

--- src/app/shared/NoteContentEllipsis.svelte
+++ src/app/shared/NoteContentEllipsis.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <div class="z-10 relative -mt-4 mb-0 flex justify-center">
-  <Anchor stopPropagation button low on:click>See more</Anchor>
+  <Anchor stopPropagation button low on:click class="!no-underline">See more</Anchor>
 </div>
