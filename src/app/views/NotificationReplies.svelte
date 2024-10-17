8a76ba01ee848cbb185fa6ea032b326da5d89827
Jon Staab <shtaab@gmail.com>
2024-10-17 12:28:34 -0700

    tweak notification copy

--- src/app/views/NotificationReplies.svelte
+++ src/app/views/NotificationReplies.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import {pluck} from "ramda"
   import {uniq} from "@welshman/lib"
+  import {pubkey} from "@welshman/app"
   import {formatTimestamp} from "src/util/misc"
   import Spinner from 'src/partials/Spinner.svelte'
   import PeopleAction from "src/app/shared/PeopleAction.svelte"
@@ -12,11 +13,13 @@
 
   const {root, interactions, timestamp} = notification
   const event = deriveEvent(root)
+
+  $: actionText = $event?.pubkey === $pubkey ? "replied to your note" : "mentioned you"
 </script>
 
 <div class="flex flex-col gap-4 my-4">
   <div class="flex items-center justify-between">
-    <PeopleAction pubkeys={uniq(pluck("pubkey", interactions))} actionText="replied to your note" />
+    <PeopleAction pubkeys={uniq(pluck("pubkey", interactions))} {actionText} />
     <small>{formatTimestamp(timestamp)}</small>
   </div>
   {#if $event}
