9912191d6ce7be3bcd8c0664a9e29049b213da22
Jon Staab <shtaab@gmail.com>
2024-10-25 16:44:06 -0700

    replace bugsnag with glitchtip

--- src/app/tracking.ts
+++ src/app/tracking.ts
@@ -1,32 +1,13 @@
-import Bugsnag from "@bugsnag/js"
+import * as Sentry from "@sentry/browser"
 
 export const setupTracking = () => {
-  if (!import.meta.env.VITE_BUGSNAG_API_KEY) return
-
-  // Initialize
-  Bugsnag.start({
-    apiKey: import.meta.env.VITE_BUGSNAG_API_KEY,
-    collectUserIp: false,
-  })
-
-  // Redact long strings, especially hex and bech32 keys which are 64 and 63
-  // characters long, respectively. Put the threshold a little lower in case
-  // someone accidentally enters a key with the last few digits missing
-  const redactErrorInfo = (info: any) =>
-    JSON.parse(
-      JSON.stringify(info || null)
-        .replace(/\d+:{60}\w+:\w+/g, "[REDACTED]")
-        .replace(/\w{60}\w+/g, "[REDACTED]"),
-    )
-
-  Bugsnag.addOnError((event: any) => {
-    // Redact individual properties since the event needs to be
-    // mutated, and we don't want to lose the prototype
-    event.context = redactErrorInfo(event.context)
-    event.request = redactErrorInfo(event.request)
-    event.exceptions = redactErrorInfo(event.exceptions)
-    event.breadcrumbs = redactErrorInfo(event.breadcrumbs)
-
-    return true
-  })
+  if (import.meta.env.VITE_SENTRY_DSN) {
+    Sentry.init({
+      dsn: import.meta.env.VITE_SENTRY_DSN,
+      tracesSampleRate: 0.01,
+      integrations(integrations) {
+        return integrations.filter(integration => integration.name !== 'Breadcrumbs')
+      },
+    })
+  }
 }
