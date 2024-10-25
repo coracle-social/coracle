136ddb773a1d08d0b59d66b27925b621b518d32e
Jon Staab <shtaab@gmail.com>
2024-10-25 16:36:59 -0700

    Turn off breadcrumbs

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ if (import.meta.env.VITE_SENTRY_DSN) {
   Sentry.init({
     dsn: import.meta.env.VITE_SENTRY_DSN,
     tracesSampleRate: 0.01,
+    integrations(integrations) {
+      return integrations.filter(integration => integration.name !== 'Breadcrumbs')
+    },
   })
 }
 
