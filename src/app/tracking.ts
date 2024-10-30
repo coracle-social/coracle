44daf9e98cf4173b4ca69691b52099387efc88eb
Jon Staab <shtaab@gmail.com>
2024-10-30 14:33:25 -0700

    Add sourcemaps script

--- src/app/tracking.ts
+++ src/app/tracking.ts
@@ -1,9 +1,9 @@
 import * as Sentry from "@sentry/browser"
 
 export const setupTracking = () => {
-  if (import.meta.env.VITE_SENTRY_DSN) {
+  if (import.meta.env.VITE_GLITCHTIP_API_KEY) {
     Sentry.init({
-      dsn: import.meta.env.VITE_SENTRY_DSN,
+      dsn: import.meta.env.VITE_GLITCHTIP_API_KEY,
       tracesSampleRate: 0.01,
       integrations(integrations) {
         return integrations.filter(integration => integration.name !== 'Breadcrumbs')
