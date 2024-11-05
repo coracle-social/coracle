1275a57139b3428602b4bd970a65d79240c2ff90
Ticruz <thib.duchene@gmail.com>
2024-11-05 13:42:29 +0100

    0 repost noshow

--- src/app/shared/NoteMeta.svelte
+++ src/app/shared/NoteMeta.svelte
@@ -9,12 +9,14 @@
   const repostPubkeys = uniq(pluck("pubkey", reposts))
 </script>
 
-<p class="flex items-center gap-1 pb-2 text-sm text-neutral-300">
-  <i class="fa fa-rotate" />
-  Reposted by
-  {#if repostPubkeys.length === 1}
-    <PersonLink pubkey={repostPubkeys[0]} />
-  {:else}
-    {repostPubkeys.length} people
-  {/if}
-</p>
+{#if repostPubkeys.length > 0}
+  <p class="flex items-center gap-1 pb-2 text-sm text-neutral-300">
+    <i class="fa fa-rotate" />
+    Reposted by
+    {#if repostPubkeys.length === 1}
+      <PersonLink pubkey={repostPubkeys[0]} />
+    {:else}
+      {repostPubkeys.length} people
+    {/if}
+  </p>
+{/if}
