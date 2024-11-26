cbe8f6bb3c8d2c40cb4e3260e12d0163d9967ad6
Jon Staab <shtaab@gmail.com>
2024-11-26 11:08:53 -0800

    Remove chat alert

--- src/app/views/ChannelsList.svelte
+++ src/app/views/ChannelsList.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
   import {onMount} from "svelte"
-  import {toTitle, Storage} from "hurdak"
+  import {toTitle} from "hurdak"
   import {derived} from "svelte/store"
   import {signer} from "@welshman/app"
-  import {slide} from "src/util/transition"
   import {createScroller} from "src/util/misc"
   import Tabs from "src/partials/Tabs.svelte"
-  import Card from "src/partials/Card.svelte"
   import Anchor from "src/partials/Anchor.svelte"
   import Popover from "src/partials/Popover.svelte"
   import FlexColumn from "src/partials/FlexColumn.svelte"
   import Content from "src/partials/Content.svelte"
   import ChannelsListItem from "src/app/views/ChannelsListItem.svelte"
   import {router} from "src/app/util/router"
-  import {channels, hasNip44, hasNewMessages, markAllChannelsRead} from "src/engine"
+  import {channels, hasNewMessages, markAllChannelsRead} from "src/engine"
 
   const activeTab = window.location.pathname.slice(1) === "channels" ? "conversations" : "requests"
   const setActiveTab = tab => {
@@ -26,18 +24,12 @@
     limit += 20
   }
 
-  const hideAlert = () => {
-    Storage.setJson("hide_nip04_alert", true)
-    hideNip04Alert = true
-  }
-
   $: tabChannels = activeTab === "conversations" ? $accepted : $requests
   $: accepted = derived(channels, $ch => $ch.filter(c => c.last_sent > 0))
   $: requests = derived(channels, $ch => $ch.filter(c => c.last_received > 0 && c.last_sent === 0))
 
   let element
   let limit = 20
-  let hideNip04Alert = Storage.getJson("hide_nip04_alert")
 
   onMount(() => {
     const scroller = createScroller(loadMore, {element, delay: 300})
@@ -51,25 +43,6 @@
 </script>
 
 <FlexColumn bind:element>
-  {#if $hasNip44 && !hideNip04Alert}
-    <div class="-my-2">
-      <div out:slide|local class="py-4">
-        <Card class="relative">
-          <FlexColumn>
-            <p class="flex items-center gap-4 text-xl">
-              <i class="fa fa-info-circle" /> You’re using a new version of private messages.
-            </p>
-            <p>
-              Nostr messages are now more secure, but are still far from perfect. For truly private
-              communication, consider using <Anchor external underline href="https://simplex.chat"
-                >SimpleX</Anchor> instead.
-            </p>
-          </FlexColumn>
-          <i class="fa fa-times absolute right-0 top-0 cursor-pointer p-2" on:click={hideAlert} />
-        </Card>
-      </div>
-    </div>
-  {/if}
   <div class="flex justify-between">
     <div class="flex items-center gap-2">
       <i class="fa fa-comments fa-lg" />
