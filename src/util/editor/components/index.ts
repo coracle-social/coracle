d49db63bf19d81a526086ff37b50c3eb93f77cd6
Jon Staab <shtaab@gmail.com>
2025-01-01 09:17:07 -0800

    Use welshman/editor

--- src/util/editor/components/index.ts
+++
@@ -1,6 +0,0 @@
-export {default as Suggestions} from "./Suggestions.svelte"
-export {default as SuggestionString} from "./SuggestionString.svelte"
-export {default as EditBolt11} from "./EditBolt11.svelte"
-export {default as EditMedia} from "./EditMedia.svelte"
-export {default as EditEvent} from "./EditEvent.svelte"
-export {default as EditMention} from "./EditMention.svelte"
--- src/util/editor/extensions/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./BreakOrSubmit.js"
-export * from "./CodeInline.js"
-export * from "./Welshman.js"
-export * from "./WordCount.js"
