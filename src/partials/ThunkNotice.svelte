5a227fcd573d96f64e30082ca426b265b861e47a
Ticruz <thib.duchene@gmail.com>
2025-01-08 17:24:26 +0100

    accomodate long url in hud console

--- src/partials/ThunkNotice.svelte
+++ src/partials/ThunkNotice.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import cx from "classnames"
   import {formatTimestamp} from "@welshman/app"
   import {messageAndColorFromStatus, type PublishNotice} from "src/domain/connection"
 
@@ -7,9 +8,11 @@
   const {color} = messageAndColorFromStatus(notice.status)
 </script>
 
-<div class="flex gap-2 p-2">
+<div class="flex flex-wrap gap-2 p-2">
   <span class="shrink-0 text-neutral-400">{formatTimestamp(notice.created_at)}</span>
-  <strong class={color}>to {notice.url}:</strong>
+  <strong
+    class={cx(color, "max-w-full overflow-hidden text-ellipsis whitespace-nowrap text-success")}
+    >to {notice.url}</strong>
   <span class="shrink-0">[Kind {notice.eventKind}]</span>
-  <span class="">{notice.message}</span>
+  <span>{notice.message}</span>
 </div>
