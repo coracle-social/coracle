8cdd112b948d42bb0514d712e227fea5453fc8f6
Ticruz <thib.duchene@gmail.com>
2025-01-14 11:27:30 +0100

    use constant kind

--- src/app/shared/NoteContentKindRelay.svelte
+++ src/app/shared/NoteContentKindRelay.svelte
@@ -2,19 +2,20 @@
   import {getRelayTagValues, isShareableRelayUrl} from "@welshman/util"
   import FlexColumn from "src/partials/FlexColumn.svelte"
   import RelayCard from "src/app/shared/RelayCard.svelte"
+  import {RELAYS, BLOCKED_RELAYS, SEARCH_RELAYS, INBOX_RELAYS} from "@welshman/util"
 
   export let note
-  export let kind: 10002 | 10006 | 10007 | 10050 = 10002
+  export let kind: 10002 | 10006 | 10007 | 10050 = RELAYS
 </script>
 
 <FlexColumn small>
-  {#if kind === 10002}
+  {#if kind === RELAYS}
     <p>New relay selections:</p>
-  {:else if kind === 10006}
+  {:else if kind === BLOCKED_RELAYS}
     <p>New blocked relay selections:</p>
-  {:else if kind === 10007}
+  {:else if kind === SEARCH_RELAYS}
     <p>New searched relay selections:</p>
-  {:else if kind === 10050}
+  {:else if kind === INBOX_RELAYS}
     <p>New inbox relay selections:</p>
   {/if}
   {#each getRelayTagValues(note.tags).filter(isShareableRelayUrl) as url}
