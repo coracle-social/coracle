34c7bfcffb7612240821c4a457f68089e613e7a6
Jon Staab <shtaab@gmail.com>
2025-05-15 15:52:17 -0700

    Get rid of overries, bump welshman to lockstep versioning

--- build-in-production.sh
+++ build-in-production.sh
@@ -6,11 +6,8 @@ git describe --tags --abbrev=0
 export VITE_BUILD_VERSION=$RENDER_GIT_COMMIT
 export VITE_BUILD_HASH=$RENDER_GIT_COMMIT
 
-# Remove link overrides
-node remove-pnpm-overrides.js package.json
-
-# When CI=true as it is on render.com, removing link overrides breaks the lockfile
-pnpm i --no-frozen-lockfile
+# Install dependencies
+pnpm i
 
 # Rebuild sharp
 pnpm rebuild
