ac756bf266bcf1cae40e4af94645313bd6425064
Jon Staab <shtaab@gmail.com>
2025-06-05 11:49:45 -0700

    tweak relay status component

--- src/app/components/SpaceRelayStatus.svelte
+++ src/app/components/SpaceRelayStatus.svelte
@@ -14,57 +14,57 @@
   const owner = $derived($relay?.profile?.pubkey)
 </script>
 
-<div class="card2 bg-alt">
-  <h3 class="mb-4 flex items-center gap-2 text-lg font-semibold">
-    <Icon icon="server" />
-    Relay Status
-  </h3>
-  <div class="flex flex-col gap-3">
+<div class="card2 bg-alt flex flex-col gap-4">
+  <div class="flex items-center justify-between">
+    <h3 class="flex items-center gap-2 text-lg font-semibold">
+      <Icon icon="server" />
+      Relay Details
+    </h3>
     <SocketStatusIndicator {url} />
-    {#if $relay?.profile}
-      {@const {software, version, supported_nips, limitation} = $relay.profile}
-      <div class="flex flex-wrap gap-1">
-        {#if owner}
-          <div class="badge badge-neutral">
-            <span class="ellipsize">Administrator: <ProfileLink unstyled pubkey={owner} /></span>
-          </div>
-        {/if}
-        {#if $relay?.profile?.contact}
-          <div class="badge badge-neutral">
-            <span class="ellipsize">Contact: {$relay.profile.contact}</span>
-          </div>
-        {/if}
-        {#if software}
-          <div class="badge badge-neutral">
-            <span class="ellipsize">Software: {software}</span>
-          </div>
-        {/if}
-        {#if version}
-          <div class="badge badge-neutral">
-            <span class="ellipsize">Version: {version}</span>
-          </div>
-        {/if}
-        {#if Array.isArray(supported_nips)}
-          <p class="badge badge-neutral">
-            <span class="ellipsize">Supported NIPs: {supported_nips.join(", ")}</span>
-          </p>
-        {/if}
-        {#if limitation?.auth_required}
-          <p class="badge badge-warning">
-            <span class="ellipsize">Auth Required</span>
-          </p>
-        {/if}
-        {#if limitation?.payment_required}
-          <p class="badge badge-warning">
-            <span class="ellipsize">Payment Required</span>
-          </p>
-        {/if}
-        {#if limitation?.min_pow_difficulty}
-          <p class="badge badge-warning">
-            <span class="ellipsize">Min PoW: {limitation?.min_pow_difficulty}</span>
-          </p>
-        {/if}
-      </div>
-    {/if}
   </div>
+  {#if $relay?.profile}
+    {@const {software, version, supported_nips, limitation} = $relay.profile}
+    <div class="flex flex-wrap gap-1">
+      {#if owner}
+        <div class="badge badge-neutral">
+          <span class="ellipsize">Administrator: <ProfileLink unstyled pubkey={owner} /></span>
+        </div>
+      {/if}
+      {#if $relay?.profile?.contact}
+        <div class="badge badge-neutral">
+          <span class="ellipsize">Contact: {$relay.profile.contact}</span>
+        </div>
+      {/if}
+      {#if software}
+        <div class="badge badge-neutral">
+          <span class="ellipsize">Software: {software}</span>
+        </div>
+      {/if}
+      {#if version}
+        <div class="badge badge-neutral">
+          <span class="ellipsize">Version: {version}</span>
+        </div>
+      {/if}
+      {#if Array.isArray(supported_nips)}
+        <p class="badge badge-neutral">
+          <span class="ellipsize">Supported NIPs: {supported_nips.join(", ")}</span>
+        </p>
+      {/if}
+      {#if limitation?.auth_required}
+        <p class="badge badge-warning">
+          <span class="ellipsize">Auth Required</span>
+        </p>
+      {/if}
+      {#if limitation?.payment_required}
+        <p class="badge badge-warning">
+          <span class="ellipsize">Payment Required</span>
+        </p>
+      {/if}
+      {#if limitation?.min_pow_difficulty}
+        <p class="badge badge-warning">
+          <span class="ellipsize">Min PoW: {limitation?.min_pow_difficulty}</span>
+        </p>
+      {/if}
+    </div>
+  {/if}
 </div>
