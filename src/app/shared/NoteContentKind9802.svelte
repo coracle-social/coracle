544c602f736dbe6013ddf2d62c7e1ff055f8f504
Jon Staab <shtaab@gmail.com>
2025-10-13 10:47:48 -0700

    Show a and e tags on highlights

--- src/app/shared/NoteContentKind9802.svelte
+++ src/app/shared/NoteContentKind9802.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+  import * as nip19 from "nostr-tools/nip19"
   import {fromPairs} from "@welshman/lib"
   import type {TrustedEvent} from "@welshman/util"
+  import {Address, getTag} from "@welshman/util"
+  import Link from "src/partials/Link.svelte"
   import NoteContentKind1 from "src/app/shared/NoteContentKind1.svelte"
   import NoteContentLinks from "src/app/shared/NoteContentLinks.svelte"
 
@@ -9,6 +12,8 @@
   export let showMedia: boolean
 
   const meta = fromPairs(note.tags)
+  const aTag = getTag("a", note.tags)
+  const eTag = getTag("e", note.tags)
 </script>
 
 <div class="flex flex-col gap-2">
@@ -20,7 +25,34 @@
       <NoteContentKind1 {note} {showEntire} />
     </div>
   </div>
-  {#if meta.r}
+  {#if aTag}
+    {@const naddr = Address.from(aTag[1], aTag.slice(2, 3)).toNaddr()}
+    <div class="flex items-center gap-1 text-end text-sm text-neutral-400">
+      <i class="fa fa-highlighter fa-xs mt-1" />
+      <Link
+        modal
+        stopPropagation
+        class="overflow-hidden text-ellipsis whitespace-nowrap underline"
+        href={naddr}>
+        {naddr.slice(0, 16) + "…"}
+      </Link>
+    </div>
+  {:else if eTag}
+    {@const id = eTag[1]}
+    {@const relays = eTag.slice(2, 3)}
+    {@const author = eTag[3]}
+    {@const nevent = nip19.neventEncode({id, relays, author})}
+    <div class="flex items-center gap-1 text-end text-sm text-neutral-400">
+      <i class="fa fa-highlighter fa-xs mt-1" />
+      <Link
+        modal
+        stopPropagation
+        class="overflow-hidden text-ellipsis whitespace-nowrap underline"
+        href={nevent}>
+        {nevent.slice(0, 16) + "…"}
+      </Link>
+    </div>
+  {:else if meta.r}
     <div class="flex items-center gap-1 text-end text-sm text-neutral-400">
       <i class="fa fa-highlighter fa-xs mt-1" />
       <NoteContentLinks urls={[meta.r]} />
